1e8d37b462d829d069d4173cf1517f30cdb07428
Kostiantyn Selivanov <k.selivanov@softonix.org>
2024-10-06 21:59:02 +0200

    Added removeDuplicates method

--- test/simple_list_extensions_test.dart
+++ test/simple_list_extensions_test.dart
@@ -1,6 +1,7 @@
 import 'extensions/add_at.dart' as add_at;
 import 'extensions/every.dart' as every;
 import 'extensions/get_chunks.dart' as get_chunks;
+import 'extensions/remove_duplicates.dart' as remove_duplicates;
 import 'extensions/replace_at.dart' as replace_at;
 import 'extensions/replace_where.dart' as replace_where;
 
@@ -8,6 +9,7 @@ void main() {
   add_at.main();
   every.main();
   get_chunks.main();
+  remove_duplicates.main();
   replace_at.main();
   replace_where.main();
 }
